985423d55e3f49cc1604a03d61016adc74ccc453
Adi Unnithan (MBO Partners) <v-adunni@microsoft.com>
2019-01-24 17:20:37 +0000

    centering pie

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import styled from 'styled-components';
 import { storiesOf } from "@storybook/react";
 import { PieSpinner } from "../components/piespinner";
 import {
@@ -11,30 +11,41 @@ import {
 } from "@storybook/addon-knobs";
 import "@storybook/addon-knobs/register";
 
-storiesOf("PieSpinner", module)
-  .addDecorator(withKnobs)
-  .add("default", () => {
-    const duration = number("Duration", 5000);
-    const bgColor = color("Background Color", "#08c");
-    const borderColor = color("Border Color", "#ccc");
-    const borderWidth = text("Border Width", "20px");
-    const fgColor = color("Foreground Color", "white");
-    const fontSize = text("Font Size", "1em");
-    const height = text("Height", "100px");
-    const isInfinite = boolean("Is Infinite", true);
-    const width = text("Width", "100px");
-    return (
-      <div>
-        <PieSpinner
-          backgroundColor={bgColor}
-          border={`${borderWidth} solid ${borderColor}`}
-          color={fgColor}
-          duration={duration}
-          fontSize={fontSize}
-          height={height}
-          width={width}
-          isInfinite={isInfinite}
-        />
-      </div>
-    );
-  });
+storiesOf('PieSpinner', module)
+    .addDecorator(withKnobs)
+    .add('default', () => {
+        const duration = number('Duration', 5000);
+        const bgColor = color('Background Color', '#08c');
+        const borderColor = color('Border Color', '#ccc');
+        const borderWidth = text('Border Width', '20px');
+        const fgColor = color('Foreground Color', 'white');
+        const fontSize = text('Font Size', '1em');
+        const height = text('Height', '500px');
+        const isInfinite = boolean('Is Infinite', true);
+        const width = text('Width', '500px');
+        return (
+            <Layout>
+                <PieSpinner
+                    backgroundColor={bgColor}
+                    border={`${borderWidth} solid ${borderColor}`}
+                    color={fgColor}
+                    duration={duration}
+                    fontSize={fontSize}
+                    height={height}
+                    width={width}
+                    isInfinite={isInfinite}
+                />
+            </Layout>
+        );
+    });
+
+const Layout = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: grid;
+    justify-items: center;
+    align-items: center;
+`;
